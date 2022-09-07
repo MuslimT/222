b31ae58d27fdd266c832851868c291c4a91fd76c
MuslimT <ingushian157@gmail.com>
2022-09-07 22:04:59 +0300

    first commit

--- src/main/java/web/config/WebConfig.java
+++ src/main/java/web/config/WebConfig.java
@@ -1,5 +1,6 @@
 package web.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -11,40 +12,41 @@ import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 
-@Configuration
-@EnableWebMvc
-@ComponentScan("web")
-public class WebConfig implements WebMvcConfigurer {
+    @Configuration
+    @EnableWebMvc
+    @ComponentScan("web")
+    public class WebConfig implements WebMvcConfigurer {
 
-    private final ApplicationContext applicationContext;
+        private final ApplicationContext applicationContext;
 
-    public WebConfig(ApplicationContext applicationContext) {
-        this.applicationContext = applicationContext;
+        @Autowired
+        public WebConfig(ApplicationContext applicationContext) {
+            this.applicationContext = applicationContext;
+        }
+
+
+        @Bean
+        public SpringResourceTemplateResolver templateResolver() {
+            SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+            templateResolver.setApplicationContext(applicationContext);
+            templateResolver.setPrefix("/WEB-INF/pages/");
+            templateResolver.setSuffix(".html");
+            return templateResolver;
+        }
+
+        @Bean
+        public SpringTemplateEngine templateEngine() {
+            SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+            templateEngine.setTemplateResolver(templateResolver());
+            templateEngine.setEnableSpringELCompiler(true);
+            return templateEngine;
+        }
+
+
+        @Override
+        public void configureViewResolvers(ViewResolverRegistry registry) {
+            ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+            resolver.setTemplateEngine(templateEngine());
+            registry.viewResolver(resolver);
+        }
     }
-
-
-    @Bean
-    public SpringResourceTemplateResolver templateResolver() {
-        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
-        templateResolver.setApplicationContext(applicationContext);
-        templateResolver.setPrefix("/WEB-INF/pages/");
-        templateResolver.setSuffix(".html");
-        return templateResolver;
-    }
-
-    @Bean
-    public SpringTemplateEngine templateEngine() {
-        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
-        templateEngine.setTemplateResolver(templateResolver());
-        templateEngine.setEnableSpringELCompiler(true);
-        return templateEngine;
-    }
-
-
-    @Override
-    public void configureViewResolvers(ViewResolverRegistry registry) {
-        ThymeleafViewResolver resolver = new ThymeleafViewResolver();
-        resolver.setTemplateEngine(templateEngine());
-        registry.viewResolver(resolver);
-    }
-}
